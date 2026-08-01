64eb8c284f1d3e3f1ca282a91eb6ea9cc6c909a9
dalgorosas <dalgoro.sas01@gmail.com>
2026-08-01 09:59:38 -0500

    DBI-ADMIN-001 integra revisión 0007 en PostGIS efímero

--- .github/scripts/ci_dbi_migration_integration.py
+++ .github/scripts/ci_dbi_migration_integration.py
@@ -43,7 +43,7 @@ ADMIN_ROLE = "postgres"
 DBI_DATABASE = "dbi_test"
 DBI_OWNER_ROLE = "dbi_test_owner"
 DBI_MIGRATOR_ROLE = "dbi_test_migrator"
-EXPECTED_HEAD = "dbi_0006_plot_boundaries"
+EXPECTED_HEAD = "dbi_0007_admin_audit"
 LEGACY_TABLES = frozenset({"documents", "users", "companies"})
 SPATIAL_CONSTRAINTS = frozenset(
     {
