a95b29dd68868360b5fcadec4f2a55873461adeb
dalgorosas <dalgoro.sas01@gmail.com>
2026-09-02 19:55:33 -0500

    test(DBI-QUEUE-001): reconocer migración durable como cabeza DBI

--- .github/scripts/ci_dbi_migration_integration.py
+++ .github/scripts/ci_dbi_migration_integration.py
@@ -43,7 +43,7 @@ ADMIN_ROLE = "postgres"
 DBI_DATABASE = "dbi_test"
 DBI_OWNER_ROLE = "dbi_test_owner"
 DBI_MIGRATOR_ROLE = "dbi_test_migrator"
-EXPECTED_HEAD = "dbi_0011_flight_manifest"
+EXPECTED_HEAD = "dbi_0012_durable_delivery"
 LEGACY_TABLES = frozenset({"documents", "users", "companies"})
 SPATIAL_CONSTRAINTS = frozenset(
     {
